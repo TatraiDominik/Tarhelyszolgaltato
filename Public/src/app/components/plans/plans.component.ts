8e5d6663d6f334ccd99a78b218505138b54ad596
Kulimák Máté <kulimakmate@turr.hu>
2025-02-05 09:07:56 +0100

    Plans frontend

--- Public/src/app/components/plans/plans.component.ts
+++ Public/src/app/components/plans/plans.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-plans',
-  standalone: true,
-  imports: [],
-  templateUrl: './plans.component.html',
-  styleUrl: './plans.component.scss'
+    selector: 'app-plans',
+    templateUrl: './plans.component.html',
+    styleUrls: ['./plans.component.css'],
 })
-export class PlansComponent {
+export class PlansComponent implements OnInit {
+    userName: string = 'Teszt Elek'; // Példa felhasználónév
+    userDomain: string = 'www.kulimakekszeresz.hu'; // Példa domain
+    currentSubscription: string = 'Alap csomag'; // Példa előfizetés
 
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+        // Példa: Adatok lekérése a backendről
+        this.fetchUserData();
+    }
+
+    fetchUserData() {
+        // Példa: HTTP kérés a backendhez
+        this.http.get('http://localhost:3000/api/user').subscribe((response: any) => {
+            this.userName = response.name;
+            this.userDomain = response.domain;
+            this.currentSubscription = response.subscription;
+        });
+    }
 }
